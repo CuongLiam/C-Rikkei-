802454b1c0bd9f28fcddc5ae6fdb931e7aa1d0ce
CuongLiam <114846183+CuongLiam@users.noreply.github.com>
2024-12-10 22:22:53 +0700

    Add files via upload

---
+++ ss2Bai4.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Nhap canh a : ");
+	scanf("%d",&a);
+	
+	printf("Dien tich hinh vuong canh a la: %d\n",a*a);
+	printf("Chu vi hinh vuong canh a la: %d",a*4);
+}
