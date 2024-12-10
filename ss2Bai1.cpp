ec315e8ab8e79cb2019d18cc7f474a2a90144dec
CuongLiam <114846183+CuongLiam@users.noreply.github.com>
2024-12-10 22:23:16 +0700

    Add files via upload

---
+++ ss2Bai1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h> //khai bao thu vien
+
+int main(){        //khai bao ham main 
+	int a = 10 ;   //khai bao va gan gia tri bien a
+	int b = 15;    //khai bao va gan gia tri bien b
+	int Sum = a+b; //khai bao va gan gia tri bien Sum = a+b
+	printf(" %d ",Sum); //out put va in ra man hinh Sum
+}
---
+++ ss2Bai2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	int a = 10 ;
+	int b = 15;
+	int Sum = a+b;
+	printf(" %d ",Sum); 
+}
