a925f6eab67755412cf41d17bae69ac2bf04b01e
CuongLiam <114846183+CuongLiam@users.noreply.github.com>
2024-12-16 19:51:56 +0700

    Add files via upload

---
+++ ss4Bai5.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, num3;
+
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &num1);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &num2);
+    printf("Nhap so thu ba: ");
+    scanf("%d", &num3);
+    
+    if ((num3 > num1 && num3 < num2) || (num3 > num2 && num3 < num1)){
+        printf("so thu ba (%d) nam trong khoang giua %d va %d.\n", num3, num1, num2);
+    } else{
+        printf("so thu ba (%d) khong nam trong khoang giua %d va %d.\n", num3, num1, num2);
+    }
+    return 0;
+}
+
