d7a6b6d9df707bd6e60e3a5a1ce23ba313cfec06
CuongLiam <114846183+CuongLiam@users.noreply.github.com>
2024-12-30 00:07:12 +0700

    Add files via upload

--- ss12Bai5.cpp
+++ ss12Bai5.cpp
@@ -26,12 +26,11 @@ int main(void){
 int isPrime(int a){
 	if(a<2){
 		return 0;
-	} else{
-		for(int i=2; i*i<=a; i++){
-			if(a % i == 0){
-				return 0;
-			}
-		}
-		return 1;
 	}
+	for(int i=2; i*i<=a; i++){
+		if(a % i == 0){
+			return 0;
+		}
+	}
+	return 1;
 };
