49a7794cab4723975ad211ff1b905ec8cf4bbd81
CuongLiam <114846183+CuongLiam@users.noreply.github.com>
2025-01-07 16:37:39 +0700

    Add files via upload

--- ss16Bai3.cpp
+++ ss16Bai3.cpp
@@ -3,11 +3,11 @@
 int sumIs(int *a, int *b, int *sum);
 int main(void){
 	int a = 10,b = 90,sum=0;
+	
 	sumIs(&a, &b, &sum);
-	printf("sum la = %d", sum);
 	
 	return 0;
 };
 int sumIs(int *a, int *b, int *sum){
-	return *sum = *a + *b;
+	printf("sum la = %d", *sum = *a + *b);
 };
